ac8b598d368aabdebe13798a8ccb911de93813a8
Jonatan A. Mancera Tabarez <jamt042831@gmail.com>
2022-03-22 09:58:37 -0300

    Update sign_up_page.dart

--- lib/pages/auth/sign_up/sign_up_page.dart
+++ lib/pages/auth/sign_up/sign_up_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-class SignUp extends HookWidget {
-  const SignUp({Key? key}) : super(key: key);
+class SignUpPage extends HookWidget {
+  const SignUpPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
