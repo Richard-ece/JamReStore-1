df302424159eadab1d27f49c134426ef074bc6a9
Jonatan A. Mancera Tabarez <jamt042831@gmail.com>
2022-03-23 10:50:03 -0300

    remove unused veriables

--- lib/bloc/auth/auth_state.dart
+++ lib/bloc/auth/auth_state.dart
@@ -3,18 +3,12 @@ import 'package:equatable/equatable.dart';
 import 'package:jam_re_store/utils/constants/enums.dart';
 
 class AuthState extends Equatable {
-  final String name;
-  final String password;
-  final String email;
   final RequestStatus signInRequestStatus;
   final RequestStatus signUpRequestStatus;
   final DioError? signInRequestError;
   final DioError? signUpRequestError;
 
   AuthState({
-    this.name = '',
-    this.password = '',
-    this.email = '',
     this.signInRequestStatus = RequestStatus.initial,
     this.signUpRequestStatus = RequestStatus.initial,
     this.signInRequestError,
@@ -22,18 +16,12 @@ class AuthState extends Equatable {
   });
 
   AuthState copyWith({
-    String? name,
-    String? password,
-    String? email,
     RequestStatus? signInRequestStatus,
     RequestStatus? signUpRequestStatus,
     DioError? signInRequestError,
     DioError? signUpRequestError,
   }) {
     return AuthState(
-      name: name ?? this.name,
-      password: password ?? this.password,
-      email: email ?? this.email,
       signInRequestStatus: signInRequestStatus ?? this.signInRequestStatus,
       signUpRequestStatus: signUpRequestStatus ?? this.signUpRequestStatus,
       signInRequestError: signInRequestError ?? this.signInRequestError,
@@ -43,9 +31,6 @@ class AuthState extends Equatable {
 
   @override
   List<Object?> get props => [
-        name,
-        password,
-        email,
         signInRequestStatus,
         signUpRequestStatus,
         signInRequestError,
