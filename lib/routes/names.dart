05b6e23b9c6ce0450d7868c8499435b3557026ca
Brayan Mancera Tabarez <brayanbmt@hotmail.com>
2022-03-25 13:21:16 +0100

    verificacion numero

--- lib/routes/names.dart
+++ lib/routes/names.dart
@@ -1,6 +1,6 @@
 class NamesRoutes {
   static const initial = "/";
-  static const signUp = "/signUp dsjaodjsa";
+  static const signUp = "/signUp";
   static const signIn = "/signIn";
   static const home = "/home";
   static const otp = "/otp";
