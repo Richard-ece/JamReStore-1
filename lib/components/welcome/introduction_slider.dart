0571ad7bab7b4530f5a01d2f27e74bdb6925a138
Jonatan A. Mancera Tabarez <jamt042831@gmail.com>
2022-03-28 01:38:22 -0300

    Add lottie, change images and text welcome

--- lib/components/welcome/introduction_slider.dart
+++ lib/components/welcome/introduction_slider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:jam_re_store/components/welcome/footer_auth.dart';
+import 'package:lottie/lottie.dart';
 
 class IntroductionSlider extends StatelessWidget {
   const IntroductionSlider({
@@ -13,36 +14,32 @@ class IntroductionSlider extends StatelessWidget {
     return IntroductionScreen(
       pages: [
         PageViewModel(
-          title: AppLocalizations.of(context)!.signUp,
-          body: 'Descripcion',
+          title: AppLocalizations.of(context)!.welcomeTittleOne,
+          body: AppLocalizations.of(context)!.welcomeDescriptionOne,
           image: Center(
-            child: Image.network(
-                "https://image.freepik.com/vector-gratis/personajes-diseno-plano-chateando-aplicacion-citas_23-2148271380.jpg",
-                height: 175.0),
+            child: Lottie.asset(
+                "assets/94323-jaroob-app-onboarding-animation.json"),
           ),
         ),
         PageViewModel(
-          title: 'Titulo 1',
-          body: 'Descripcion 1',
+          title: AppLocalizations.of(context)!.welcomeTittleTwo,
+          body: AppLocalizations.of(context)!.welcomeDescriptionTwo,
           image: Center(
-            child: Image.network(
-                "https://image.freepik.com/vector-gratis/personajes-diseno-plano-chateando-aplicacion-citas_23-2148271380.jpg",
-                height: 175.0),
+            child: Lottie.asset("assets/86590-growing-plant.json"),
           ),
         ),
         PageViewModel(
-          title: 'Titulo 2',
-          body: 'Descripcion 2',
+          title: AppLocalizations.of(context)!.welcomeTittleThree,
+          body: AppLocalizations.of(context)!.welcomeDescriptionThree,
           image: Center(
-            child: Image.network(
-                "https://image.freepik.com/vector-gratis/personajes-diseno-plano-chateando-aplicacion-citas_23-2148271380.jpg",
-                height: 175.0),
+            child: Lottie.asset("assets/88958-shopping-green.json"),
           ),
         ),
       ],
       showSkipButton: false,
       showDoneButton: false,
-      showNextButton: false,
+      showNextButton: true,
+      next: Text(AppLocalizations.of(context)!.next),
       globalFooter: const FooterAuth(),
     );
   }
