9f8a84c17fa9258227d45e8eeb6651f8c4cd8164
Jonatan A. Mancera Tabarez <jamt042831@gmail.com>
2022-03-27 23:09:24 -0300

    fix error routes names

--- lib/routes/names.dart
+++ lib/routes/names.dart
@@ -1,5 +1,5 @@
 class NamesRoutes {
-  static const welcome = "/";
+  static const welcome = "/welcome";
   static const signUp = "/signUp";
   static const signIn = "/signIn";
   static const home = "/home";
