981b78aa47559ba060c8fbd3069dbfc55bfd70f7
Jonatan A. Mancera Tabarez <jamt042831@gmail.com>
2022-03-29 13:07:16 -0300

    inital home tabs

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,16 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:jam_re_store/utils/helpers/app_locale.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends HookWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  Widget build(BuildContext context) {
+    ValueNotifier<int> tabIndex = useState(0);
+
+    List<Widget> listScreens = [
+      Text("1"),
+      Text("2"),
+      Text("3"),
+    ];
+
+    print("dentro");
+
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home'),
+        ),
+        body: listScreens[tabIndex.value],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: tabIndex.value,
+          onTap: (int index) {
+            tabIndex.value = index;
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: "asdas",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: "asdas",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: "asdas",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
-class _HomePageState extends State<HomePage> {
+class ChangeLenguage extends StatelessWidget {
+  const ChangeLenguage({Key? key}) : super(key: key);
+
   bool _changeLenguage(AppLocale language, bool value) {
     value == true
         ? language.changeLocale(const Locale('en'))
@@ -20,23 +63,16 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    var language = Provider.of<AppLocale>(context);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Column(
-        children: [
-          Text(AppLocalizations.of(context)!.signUp),
-          Switch(
-            onChanged: (bool value) {
-              _changeLenguage(language, value);
-            },
-            value: language.locale == const Locale('en'),
-          ),
-        ],
-      ),
+    AppLocale language = Provider.of<AppLocale>(context);
+    return Column(
+      children: [
+        Switch(
+          onChanged: (bool value) {
+            _changeLenguage(language, value);
+          },
+          value: language.locale == const Locale('en'),
+        ),
+      ],
     );
   }
 }
