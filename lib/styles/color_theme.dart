6756898b67eb489701ee898c08b0c0fbe69f9443
Brayan Mancera Tabarez <brayanbmt@hotmail.com>
2022-04-12 13:54:05 +0200

    add shades colors

--- lib/styles/color_theme.dart
+++ lib/styles/color_theme.dart
@@ -15,14 +15,32 @@ abstract class ColorTheme {
   static const lightYellow = Color(0xffFFF9E6);
   static const lightGreen = Color(0xffE6FFF0);
 
-  //Icon Colors
-  static const grey2 = Color(0xff949494);
-
-  //Text Colors
+  //Shades
   static const textBlack = Color(0xff141417);
   static const text = Color(0xff3b3b3b);
 
-  // static const grey = Color(0xff606068);
   static const grey = Color(0xff7D8D9B);
-  static const grey3 = Color(0xffcccccd);
+  static const grey1 = Color(0xffF2F5F7);
+  static const grey2 = Color(0xffD8E0E6);
+  static const grey3 = Color(0xffBECBD5);
+  static const grey4 = Color(0xffA5B6C5);
+  static const grey5 = Color(0xff8BA1B4);
+  static const grey6 = Color(0xff718CA3);
+  static const grey7 = Color(0xff5C778E);
+  static const grey8 = Color(0xff4B6174);
+  static const grey9 = Color(0xff3A4C5A);
+  static const grey10 = Color(0xff2A3640);
+  static const grey11 = Color(0xff192027);
+
+  static const blue1 = Color(0xffECF5FD);
+  static const blue2 = Color(0xffC5E1F9);
+  static const blue3 = Color(0xff9FCDF5);
+  static const blue4 = Color(0xff78B9F1);
+  static const blue5 = Color(0xff51A6ED);
+  static const blue6 = Color(0xff2B92E9);
+  static const blue7 = Color(0xff167CD4);
+  static const blue8 = Color(0xff1266AE);
+  static const blue9 = Color(0xff0E4F87);
+  static const blue10 = Color(0xff0A3960);
+  static const blue11 = Color(0xff06223A);
 }
