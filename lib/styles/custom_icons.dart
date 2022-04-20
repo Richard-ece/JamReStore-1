4f30e7aa2e20840a49a6f0471fff962175d19203
Jonatan A. Mancera Tabarez <jamt042831@gmail.com>
2022-04-20 13:12:01 -0300

    Add custom icons

---
+++ lib/styles/custom_icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const _kFontFam = 'CustomIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData eye_closed =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData eye_open =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
